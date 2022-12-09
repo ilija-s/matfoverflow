51bfa749ac70202b1dd8784010571fa131909fb8
ajili <ilijastc062p@gmail.com>
2022-12-09 15:21:43 +0100

    New: Added skeleton code for each request

--- backend/question-service/routes/question.js
+++ backend/question-service/routes/question.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const questionsRoute = express.Router();
 
+questionsRoute.get("/", (req, res) => {
+    const questions = [{
+		id: "id",
+        title: "What is a microservice architecture?",
+        description: "Description of the question...",
+        author: "Ilija Stojanovic"
+    }];
+    res.json(question);
+})
+
 questionsRoute.get("/:id", (req, res) => {
 	const id = req.params.id;
     const question = {
@@ -11,6 +21,27 @@ questionsRoute.get("/:id", (req, res) => {
         author: "Ilija Stojanovic"
     };
     res.json(question);
+})
+
+questionsRoute.post("/", (req, res) => {
+	const body = req.body;
+    return "POST REQUEST";
+})
+
+questionsRoute.put("/:id", (req, res) => {
+    const id = req.params.id;
+    return "PUT REQUEST";
+})
+
+questionsRoute.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    return "DELETE REQUEST";
+})
+
+questionsRoute.post("/:id/vote", (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    return "POST REQUEST on questions/" + id + "/vote";
 })
 
 module.exports = questionsRoute
