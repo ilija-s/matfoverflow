52ba4359c8ffb07589d771a67b802ebba5f478ac
ajili <ilijastc062p@gmail.com>
2022-12-09 20:04:25 +0100

    New: Implemented getById, getByTag, getAll and save for question service

--- backend/question-service/routes/question.js
+++ backend/question-service/routes/question.js
@@ -1,31 +1,43 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const questionsRoute = express.Router();
 
-questionsRoute.get("/", (req, res) => {
-    const questions = [{
-		id: "id",
-        title: "What is a microservice architecture?",
-        description: "Description of the question...",
-        author: "Ilija Stojanovic"
-    }];
+const QuestionModel = require("../models/question");
+
+questionsRoute.get("/", async (req, res) => {
+    const questions = await QuestionModel.getAllQuestions();
+    
+    res.json(questions);
+})
+
+questionsRoute.get("/:id", async (req, res) => {
+	const id = mongoose.Types.ObjectId(req.params.id);
+    const question = await QuestionModel.getQuestionById(id);
+
+    if (question === null) {
+        res.status(404).json("Question with given id is not found.")
+    }
+
     res.json(question);
 })
 
-questionsRoute.get("/:id", (req, res) => {
-	const id = req.params.id;
-    const question = {
-		id,
-        title: "What is a microservice architecture?",
-        description: "Description of the question...",
-        author: "Ilija Stojanovic"
-    };
-    res.json(question);
+questionsRoute.get("/tags/:tag", async (req, res) => {
+    const questions = await QuestionModel.getQuestionsByTag(req.params.tag);
+
+    res.json(questions);
 })
 
-questionsRoute.post("/", (req, res) => {
-	const body = req.body;
-    return "POST REQUEST";
+questionsRoute.post("/", async (req, res) => {
+    const { title, description, author, tags } = req.body;
+    if (!title || !description || !author || !tags) {
+        res.status(400).json("Information provided is not valid!");
+        return;
+    }
+
+    const createdQuestion = await QuestionModel.addNewQuestion({ title, description, author, tags });
+
+    res.json(createdQuestion);
 })
 
 questionsRoute.put("/:id", (req, res) => {
