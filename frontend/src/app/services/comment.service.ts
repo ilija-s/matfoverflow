5c5a658e25518534ba67814c7bf2a442cf0193b7
idzii <paunovickkp@gmail.com>
2023-01-17 19:24:03 +0100

    Refactor: services/comment.service.ts

--- frontend/src/app/services/comment.service.ts
+++ frontend/src/app/services/comment.service.ts
@@ -9,10 +9,12 @@ import { Comment } from '../comments/models/comment.model';
 })
 export class CommentService {
 
+	private readonly url = "http://localhost:4001/comments/";
+
 	constructor(private http : HttpClient) {}
 
 	public getComments(questionId : string) : Observable<Comment[]>{
-		return this.http.get<Comment[]>("http://localhost:4001/comments/" + questionId);
+		return this.http.get<Comment[]>(this.url + questionId);
 	}
 
 	private mongoObjectId = function () : string {
@@ -24,24 +26,24 @@ export class CommentService {
 
 	public upvote(commentId : string) : Observable<{currentVoteCount : number}> {
 		const userId : string = this.mongoObjectId();
-		return this.http.put<{currentVoteCount : number}>("http://localhost:4001/comments/" + commentId + "/upvote", {userId : userId});
+		return this.http.put<{currentVoteCount : number}>(this.url + commentId + "/upvote", {userId : userId});
 	}
 
 	public downvote(commentId : string) : Observable<{currentVoteCount : number}> {
 		const userId : string = this.mongoObjectId();
-		return this.http.put<{currentVoteCount : number}>("http://localhost:4001/comments/" + commentId + "/downvote", {userId : userId});
+		return this.http.put<{currentVoteCount : number}>(this.url + commentId + "/downvote", {userId : userId});
 	}
 
 	public deleteComment(commentId : string) : Observable<any> {
 		const userId : string = this.mongoObjectId();
-		return this.http.delete<any>("http://localhost:4001/comments/" + commentId);
+		return this.http.delete<any>(this.url + commentId);
 	}
 
 	public editComment(commentId : string, authorId : string, content : string) : Observable<Comment> {
-		return this.http.put<Comment>("http://localhost:4001/comments/" + commentId, {content : content, authorId : authorId});
+		return this.http.put<Comment>(this.url + commentId, {content : content, authorId : authorId});
 	}
 
 	public postComment(questionId : string, authorId : string, authorName: string, content : string) : Observable<Comment> {
-		return this.http.post<Comment>("http://localhost:4001/comments/" + questionId, {authorId : authorId, authorName : authorName, content : content});
+		return this.http.post<Comment>(this.url + questionId, {authorId : authorId, authorName : authorName, content : content});
 	}
 }
