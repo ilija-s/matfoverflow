ee2bcc425ad877d7dcd97e211190423e20063960
ajili <ilijastc062p@gmail.com>
2023-01-17 12:45:56 +0100

    New: Implemented user service

---
+++ frontend/src/app/user/services/user.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap, map } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { JwtService } from 'src/app/services/jwt.service';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private readonly urls = {
+    patchUser: "http://localhost:3000/users",
+  }
+
+  constructor(private http: HttpClient,
+              private jwtService: JwtService,
+              private authService: AuthService) { }
+
+  public patchUserData(username: string, email: string, name: string): Observable<User> {
+    const body = {username, email, name};
+    const headers: HttpHeaders = new HttpHeaders().append("Authorization", `Bearer ${this.jwtService.getToken()}`);
+
+    return this.http.patch<{token: string}>(this.urls.patchUser, body, {headers}).pipe(
+        tap((response: {token: string}) => this.jwtService.setToken(response.token)),
+        map((response: {token: string}) => this.authService.sendUserDataIfExists()!)
+    )
+  }
+
+}
