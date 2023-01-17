ce64f72e5f18c4219eb7594c520d1b93a65ddb63
ajili <ilijastc062p@gmail.com>
2023-01-17 12:45:40 +0100

    New: User info component is subscribed to  changes on user

--- frontend/src/app/user/user-info/user-info.component.ts
+++ frontend/src/app/user/user-info/user-info.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 import { User } from '../models/user.model';
 
 @Component({
@@ -6,13 +8,25 @@ import { User } from '../models/user.model';
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.css']
 })
-export class UserInfoComponent implements OnInit{
-  
-  public user: User | undefined;
+export class UserInfoComponent implements OnInit, OnDestroy {
+  public sub: Subscription;
+  public user: User | null = null;
   public isLogin: boolean = true;
+
+  constructor(private authService: AuthService) {
+    this.sub = this.authService.user.subscribe((user: User | null) => {
+      this.user = user;
+    });
+    this.authService.sendUserDataIfExists();
+  }
   
   ngOnInit(): void {
+  }
 
+  ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
   switchBetweenLoginAndRegistration(): void {
